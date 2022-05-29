a29e4334f358164df1a5882584469aafd82cf66f
rabiel <rabiel@gmail.com>
2022-05-29 23:05:33 +0200

    [Catalog-1] products.html

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products!:Array<any>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products==[
+      {id:1, name:"Computer",price:6500},
+      {id:1, name:"Printer",price:150},
+      {id:1, name:"Smart Phone",price:500}
+    ]
   }
 
 }
