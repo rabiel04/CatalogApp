eeebabf51bd1b462eab40f5aafd14beb01dde434
rabiel <rabiel@gmail.com>
2022-05-30 14:46:39 +0200

    [Catalog-1] reform1

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductsService} from "../services/products.service";
+import {Product} from "../model/product.model";
 
 @Component({
   selector: 'app-products',
@@ -8,7 +9,7 @@ import {ProductsService} from "../services/products.service";
 })
 export class ProductsComponent implements OnInit {
 
-  products!:Array<any>;
+  products!:Array<Product>;
   errorMessage!: string ;
 
   constructor(private productService:ProductsService) {
@@ -16,19 +17,28 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productService.getAllProducts().subscribe(
-   (data)=>{
-         this.products=data;
-    },
-    (err)=>{
-      this.errorMessage=err;
-    }
-    );
-
+    this.handleGetAllProducts();
   }
 
-  handleDeleteProduct(p: any) {
-    let index = this.products.indexOf(p);
-    this.products.splice(index,1)
+  handleGetAllProducts(){
+    this.productService.getAllProducts().subscribe(
+      (data)=>{
+        this.products=data;
+      },
+      (err)=>{
+        this.errorMessage=err;
+      }
+    );
+  }
+
+  handleDeleteProduct(p: Product) {
+    let conf=confirm("est vous sure ?");
+    if(!conf) return;
+    this.productService.deleteProduct(p.id).subscribe(
+      data=>{
+        let index = this.products.indexOf(p);
+        this.products.splice(index,1);
+      }
+    );
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import {Observable, of, throwError, throwIfEmpty} from "rxjs";
+import {Product} from "../model/product.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-products! :Array<any>
+products! :Array<Product>
   constructor() {
     this.products=[
       {id:1, name:"Computer",price:6500},
@@ -13,11 +14,15 @@ products! :Array<any>
       {id:3, name:"Smart Phone",price:500}]
   }
 
-  public getAllProducts() :Observable<Array<any>>{
+  public getAllProducts() :Observable<Array<Product>>{
   let rnd= Math.random()
-    if(rnd<0.5)
+    if(rnd<0.1)
       return throwError(()=>("Probleme de Connextion !!!"));
     else
       return of(this.products);
+  }
+  public deleteProduct(id :number) : Observable<boolean>{
+     this.products=this.products.filter(p=>p.id!=id)
+    return of(true)
   }
 }
